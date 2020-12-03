f5aa96059e9dbbce6f6743b85afc1b3986236a1a
dixroby arone cuellar <alestilodefe@gmail.com>
2020-12-02 21:32:17 -0500

    Feat: Use UUID instead of id as primary key

--- estudiantes/models.py
+++ estudiantes/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+import uuid
 
 class Estudiante(models.Model):
+    id = models.UUIDField(default = uuid.uuid4,primary_key=True)
     nombre = models.CharField(max_length=200)
     apellidos = models.CharField(max_length=200)
     documento = models.CharField(max_length=8)
